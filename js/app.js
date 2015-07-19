5ebf7350d99fbc1daf5f8c551923455811382709
Daniel Gorelick <dqgorelick@gmail.com>
2015-07-19 12:17:30 -0400

    working for ordered lists

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+
 function getting(){
 	$("#response").html("<h2>loading</h2>");
-	var req = new XMLHttpRequest();
-	req.onload = function(){
-		$("#response").html(req.response);
-	}
-	req.open("GET","/api",true);
-	req.send();
+
+	$.ajax({
+		url: '/api',
+		success: function(data){
+			var results = data;
+			$("#response").html(results);
+		}
+	});
+
+	// var url = "/api";
+	// var req = new XMLHttpRequest();
+	// req.onload = function(){
+	// 	$("#response").html(req.response);
+	// }
+	// req.open("GET", url, true);
+	// req.send();
 }
--- server.js
+++ server.js
@@ -2,43 +2,62 @@ var fs 				= require('fs');
 var request 		= require('request')
 var cheerio		 	= require('cheerio');
 var _ 				= require('underscore');
+var express 		= require('express');
+var app 			= express();
+var port 			= 8000;
 
+/**********************
+	Server
+**********************/
+app.use('/', express.static(__dirname+'/'));
 
-var getData = function(url){
+app.get('/api', function(req, res){
+	getData("https://en.wikipedia.org/wiki/NASA",function(data){
+		res.send(data);
+	})
+});
+
+app.listen(port, function(){
+	console.log("Listening on port " + port);
+});
+
+/**********************
+	Wiki Proxy
+**********************/
+var getData = function(url, cb){
 	request(url, function(err, response, data){
 		if (err) console.log(err);
 		if (!err && response.statusCode == 200){
-			console.log("hello")
 			$ = cheerio.load(data);
 			var filtered = [];
+			var checking = [];
 			var links = $('#bodyContent p a');
 			var body = $('#mw-content-text').children('p').text();
 			var page = $("h1").text();
-
-			console.log("          ")
-			console.log("NEW BATCH:")
-			console.log("          ")
-
+			console.log("Requested " + page + "...");
 			links.each(function(index, link){
-				var title = $(link).text();
+				var result = {};
+				result.title = $(link)[0].attribs.title;
 				if ($(link)[0].attribs.title !== undefined){
-					// if (_.contains(filtered, title) === false){
-					if (filtered.indexOf(title) === -1) {
-						filtered.push(title);
+					if (checking.indexOf(result.title) === -1) {
 						var occurences = getCount($(link).text(), body);
 						if (occurences > 0) {
-							console.log(occurences + " >>>> " + $(link)[0].attribs.title );
+							result.count = occurences;
 						}
+						filtered.push(result);
+						// find a better way to do this w/o creating another array
+						checking.push(result.title);
 					}
 				}
 			})
+			filtered.push({title:page,count:getCount(page, body)})
+			// console.log(getCount(page, body) + " >>>> " + page);
+			// console.log("There were " + filtered.length + " links.");
 
-			console.log(getCount(page, body) + " >>>> " + page);
-			console.log("There were " + filtered.length + " links.");
-
-			var query = new RegExp('\\b'+page+'\\b',"gi");
-			console.log("page title: " + page)
-			console.log("RegExp " + query);
+			var ordered = _.sortBy(filtered, function(it){return -it.count});
+			var dataToSend = JSON.stringify(ordered);
+			cb(dataToSend);
+			console.log("SENT " + filtered.length + " links\n");
 		}
 	});
 }
@@ -51,5 +70,3 @@ var getCount = function(regexp, string){
 	}
 	return matches;
 };
-
-getData("https://en.wikipedia.org/wiki/NASA");
