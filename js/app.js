45dea3f2ff23e721cb3c480dcdb857de01f561de
Daniel Gorelick <dqgorelick@gmail.com>
2015-07-20 08:53:24 -0400

    working d3

--- js/app.js
+++ js/app.js
@@ -5,16 +5,48 @@ function getting(){
 	$.ajax({
 		url: '/api',
 		success: function(data){
-			var results = data;
-			$("#response").html(results);
+			var results = JSON.parse(data);
+			console.log(results);
+      render(results);
+			$("#response").html(" ");
 		}
 	});
+}
 
-	// var url = "/api";
-	// var req = new XMLHttpRequest();
-	// req.onload = function(){
-	// 	$("#response").html(req.response);
-	// }
-	// req.open("GET", url, true);
-	// req.send();
+function render(data){
+  var w = 500;
+  var h = data.length*20;
+  var width_factor = 4;
+  var padding = 2;
+  var dataset = [5, 10, 13, 19, 25, 11, 25, 22, 18, 7];
+
+  var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+  svg.selectAll("rect").data(data).enter().append("rect").attr({
+    x: 0,
+    y: function(d, i) {
+      return i * h/data.length;
+    },
+    height: h / data.length - padding,
+    width: function(d) {
+      return d.count * width_factor;
+    },
+    fill: function(d) {
+      return "rgb(" + d.count * 10 + ", 0, 0)";
+    }
+  });
+
+  svg.selectAll("text").data(data).enter().append("text").text(function(d) {
+    return d.count + " " + d.title;
+  }).attr({
+    "text-anchor": "left",
+    y: function(d, i) {
+      return (i+0.75) * (h / data.length);
+    },
+    x: function(d) {
+      return d.count * width_factor;
+    },
+    "font-family": "sans-serif",
+    "fill": "#000000"
+  });
 }
